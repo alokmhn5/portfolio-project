6da7ebfe013d47074bc64647f03d88dd3d8184a9
ALok Mohan <alokmhn5@gmail.com>
2018-06-03 15:13:07 +0530

    created homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    topic = models.CharField(max_length = 255)
+    created_on = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+#add  the blog app to settings
+#Create a migration
+#migrate
+#register to admin
